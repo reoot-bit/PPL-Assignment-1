18e25cd232991e9d33294a95222407475fcb0ecb
mayaharach-lgtm <maya.harach@gmail.com>
2026-04-05 18:01:04 +0300

    part2 finished

--- src/part2.ts
+++ src/part2.ts
@@ -1,14 +1,32 @@
 import * as R from "ramda";
-
 const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+
+export const countVowels = (s: string): number => {
+  return R.pipe(
+    R.toLower,                        
+    stringToArray,       
+    R.filter((char: string) => vowels.includes(char)),   
+    R.length                           
+  )(s);
+};
+
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
-  
+export const isPalindrome = (text: string): boolean => {
+    const arr= R.pipe(
+        R.toLower,
+        R.replace(/[^a-z0-9]/g, ""),
+        stringToArray
+     )(text);
+    const length = arr.length;
+    const myEquals = (c: string, index: number, arr: string[]): boolean => {
+    return arr[index] === arr[arr.length - index - 1];
+    };
+    return arr.every(myEquals);
+}
 
 /* Question 2.3 */
 export type WordTree = {
@@ -16,4 +34,9 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+ export const treeToSentence = (t: WordTree): string => {
+  return R.pipe(
+    (tree: WordTree) => R.prepend(tree.root, R.map(treeToSentence, tree.children)),
+    R.join(" ")
+  )(t);
+};
